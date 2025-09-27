13572f3241f4873d48b3691304e93c1fb492e60f
haariswaqas <haarisali9@gmail.com>
2025-09-27 17:26:54 +0000

    chat_models added to admin.py

--- digitalcare/api/admin.py
+++ digitalcare/api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import (User, Otp, AdultProfile, VisitorProfile, StudentProfile, DoctorProfile, HealthCard, 
-                     Facility, Notification, Appointment, Consultation, Prescription, Symptom, VideoConsultation)
+                     Facility, Notification, Appointment, ChatRoom, ChatNotification, Message, MessageReadStatus, Consultation, Prescription, Symptom, VideoConsultation)
 # Register your models here.
 reg = admin.site.register
 
@@ -21,3 +21,7 @@ reg(Notification)
 reg(Prescription)
 reg(Symptom)
 reg(VideoConsultation)
+reg(ChatRoom)
+reg(ChatNotification)
+reg(Message)
+reg(MessageReadStatus)
